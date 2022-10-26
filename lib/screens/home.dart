e24442fa4d8b58bb2d3fe572a7f5da3482151e6a
Merwan <merwanlaouini@gmail.com>
2022-10-27 01:24:14 +0200

    style de la barre de recherche

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,24 +51,38 @@ class Home extends StatelessWidget {
                 Expanded(
                   child: Container(
                     margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
-                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
-                    decoration:
-                        BoxDecoration(color: Colors.white, boxShadow: const [
-                      BoxShadow(
-                          color: Colors.white,
-                          offset: Offset(0.0, 0.0),
-                          blurRadius: 10.0,
-                          spreadRadius: 0.0)
-                    ],
-                    borderRadius: BorderRadius.circular(10),
-
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0)
+                      ],
+                      borderRadius: BorderRadius.circular(10),
                     ),
                     child: TextField(
                       decoration: InputDecoration(
                         hintText: 'Ajoute une nouvelle tache',
                         border: InputBorder.none,
-
                       ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10
                     ),
                   ),
                 )
