9f9ca6398d828a09c0a48c22381138c80347a82c
Merwan <merwanlaouini@gmail.com>
2022-10-26 19:11:27 +0200

    Ajout de la bar de recherche (non fonctionnel)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,30 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
+import '../widgets/todo_item.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: tdBGcolor,
-          title:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset('assets/images/albumTest.jpeg')),
-            )
-          ])),
+      backgroundColor: tdBGcolor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text("ceci est la page d'acceuil"),
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      'Toutes mes taches',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  TodoItem(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Ranger le salon',
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+        backgroundColor: tdBGcolor,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/albumTest.jpeg')),
+          )
+        ]));
   }
 }
