ae69a9bf5b64b3daf7fe300f6b6874fb929072df
Merwan <merwanlaouini@gmail.com>
2022-10-27 01:16:18 +0200

    Ajout de la bar de recherche

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,7 +5,7 @@ import 'package:todo_app/model/todo.dart';
 import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
-   Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   final todoList = Todo.todoList();
 
@@ -14,31 +14,68 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGcolor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: Text(
-                      'Toutes mes taches',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'Toutes mes taches',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (Todo todoo in todoList)
+                        TodoItem(
+                          todo: todoo,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                    decoration:
+                        BoxDecoration(color: Colors.white, boxShadow: const [
+                      BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Ajoute une nouvelle tache',
+                        border: InputBorder.none,
+
                       ),
                     ),
                   ),
-                  for(Todo todoo in todoList)
-                  TodoItem(todo: todoo,),
-                ],
-              ),
-            )
-          ],
-        ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
